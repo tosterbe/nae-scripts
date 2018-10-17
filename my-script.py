0564f94a0547564138d35e00adfcc562931c4bc9
tosterbe <todd@theosterbergs.com>
2018-10-17 16:42:05 -0700

    bad-test.py
    my great script

---
+++ my-script.py
@@ -0,0 +1,40 @@
+ # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+Manifest = {
+    'Name': 'Interface_Tx_Monitor',
+    'Description': 'Tx Monitor',
+    'Version': '1.0',
+    'Author': 'malicious'
+}
+
+ParameterDefinitions = {
+    'interface_id': {
+        'Name': 'Interface Id',
+        'Description': 'Interface to be monitored',
+        'Type': 'string',
+        'Default': '1/1/52'
+    },
+
+    'threshold_limit': {
+        'Name': 'Threshold Limit',
+        'Description': 'Threshold to be monitored',
+        'Type': 'integer',
+        'Default': '1'
+
+    }
+}
